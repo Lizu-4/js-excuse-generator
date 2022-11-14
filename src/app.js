31540e20e99ffc89cf8f5833387e519d3cab305f
Lizu-4 <lizugas2002@gmail.com>
2022-11-14 03:48:08 +0000

    fiished

--- src/app.js
+++ src/app.js
@@ -8,4 +8,21 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = excuseGenerator();
+
+  function excuseGenerator() {
+    let who = ["the dog", "my granma", "his turtle", "my bird"];
+    let what = ["eat", "pissed", "crushed", "broked"];
+    let when = [
+      "before the class",
+      "right in time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    return `${who[Math.floor(Math.random() * who.length)]} ${
+      what[Math.floor(Math.random() * what.length)]
+    } ${when[Math.floor(Math.random() * when.length)]}`;
+  }
 };
