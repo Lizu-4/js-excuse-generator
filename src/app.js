451dc5e2b128122edeaeea8e779b257da67dd52d
Lizu-4 <lizugas2002@gmail.com>
2022-11-22 05:00:41 +0000

    la pag no me dejaba entregar la tarea si no era un snapshot :c

--- src/app.js
+++ src/app.js
@@ -8,11 +8,12 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-  document.getElementById("excuse").innerHTML = excuseGenerator();
+  // document.getElementById("excuse").innerHTML = excuseGenerator();
+  excuseGenerator();
 
   function excuseGenerator() {
     let who = ["the dog", "my granma", "his turtle", "my bird"];
-    let what = ["eat", "pissed", "crushed", "broked"];
+    let what = ["ate", "pissed", "crushed", "broked"];
     let when = [
       "before the class",
       "right in time",
@@ -20,9 +21,14 @@ window.onload = function() {
       "during my lunch",
       "while I was praying"
     ];
-
-    return `${who[Math.floor(Math.random() * who.length)]} ${
-      what[Math.floor(Math.random() * what.length)]
-    } ${when[Math.floor(Math.random() * when.length)]}`;
+    document.getElementById("excuse").innerHTML = `${
+      who[Math.floor(Math.random() * who.length)]
+    } ${what[Math.floor(Math.random() * what.length)]} ${
+      when[Math.floor(Math.random() * when.length)]
+    }`;
   }
 };
+
+// ${who[Math.floor(Math.random() * who.length)]} ${
+//   what[Math.floor(Math.random() * what.length)]
+// } ${when[Math.floor(Math.random() * when.length)]}
